fa21c73dd32bcb85231a7ebf8db9304a9159266b
Ashutosh verma <ashutoshrock24@gmail.com>
2023-03-06 22:10:04 +0530

    update

---
+++ Desktop/my-aap/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Function from "./Function";
+function App() {
+  return (
+    <Function/>
+  );
+}
+
+export default App;
---
+++ Desktop/my-aap/src/Function.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+    const Function=() =>{
+        return(
+            <div>
+                <button><a href="https://pluralsight.com">Visit Pluralsight</a></button>
+                
+            </div>
+        )
+    }
+
+
+export default Function;
